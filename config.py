4fbfa67faba053513c3204e7cfc6bbc7fc5c1096
Dmitry Karakutsea <92888174+Dimaskas97@users.noreply.github.com>
2021-12-10 18:35:59 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+
+
+TOKEN = '2132622817:AAERWPT4LOAx8-dg4NN4l462g1ViTQI4TSs'
+Short_project = 'https://docs.google.com/document/d/1LmH3YlPP8qo6yrF-dI58g6p6chTqPKWH0UrDDaccM2Q/edit'
+Long_project = 'https://docs.google.com/document/d/10Zntepf7AzpcyUmN-QomBEfUo_U0JcwBSEiwzlv2d54/edit'
